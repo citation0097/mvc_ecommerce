737a1a7b61cb03e7d4f59557f739453fbe0fe765
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-12-02 21:07:57 -0800

    ManagerMainAction

---
+++ src/com/kimhank/ecom/ManagerMainAction.java
@@ -0,0 +1,16 @@
+package com.kimhank.ecom;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ManagerMainAction implements CommandAction {
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response)
+			throws Throwable{
+		
+		request.setAttribute("type", new Integer(0));
+		return "/manager/managerMain.jsp";
+	}
+}
